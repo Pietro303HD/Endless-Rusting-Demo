a1a40a84b6bf2d0fccaa1df431f9aef80fab1781
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-11-24 11:29:23 -0300

    Update UpgradeModule.java

--- src/rusting/ui/dialog/research/UpgradeModule.java
+++ src/rusting/ui/dialog/research/UpgradeModule.java
@@ -2,13 +2,15 @@ package rusting.ui.dialog.research;
 
 import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
+import rusting.world.modules.ResearchModule;
 
+/*
+ * This really isn't a Dialog nor a Module.
+ * My naming just sucks.
+ * @author Pietro303HD
+ */
 public class UpgradeModule{
     public Seq<UpgradeNode> nodes = Seq.with();
-
-    public UpgradeModule(){
-        super();
-    }
 
     public void add(UpgradeNode node){
         nodes.add(node);
@@ -16,10 +18,15 @@ public class UpgradeModule{
 
     public static class UpgradeNode{
         public UpgradeNode parent = null;
-        public Content value = null;
+        public ResearchModule value = null;
 
-        public UpgradeNode(Content value){
+        public UpgradeNode(ResearchModule value){
             this.value = value;
-        };
+        }
+
+        public UpgradeNode(ResearchModule value, UpgradeNode parent){
+            this(value);
+            this.parent = parent;
+        }
     }
 }
